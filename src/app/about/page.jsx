649b0fe861d6e35cfed703cc368397dc2427fb4d
Mahder Desta <mahderds2013@gmail.com>
2023-03-25 14:39:49 +0300

    create the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl space-y-4 xl:mx-auto  mx-4">
+      <h1 className="text-2xl font-medium text-amber-600 ">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi
+        consequuntur explicabo nihil repellendus qui a, adipisci, expedita
+        quidem blanditiis pariatur corporis facilis cupiditate, laboriosam earum
+        asperiores. Corporis, soluta. Libero id veniam excepturi optio
+        exercitationem iusto consequatur dolorum qui nihil beatae in ad harum
+        omnis non deserunt, incidunt hic magni quibusdam fuga minima impedit ex.
+       
+      </p>
+      <p>
+      Ipsa recusandae nihil nemo commodi fugiat ab, dolorem voluptas omnis, et
+        nam sint saepe iusto corrupti impedit nesciunt eaque consectetur rerum
+        expedita quia! Voluptatem magnam itaque repellendus nostrum mollitia
+        laborum placeat consequatur blanditiis. Dolorem, sequi voluptates
+        labore, dolor doloremque accusamus vitae consequatur, earum minus
+        dolores asperiores!
+      </p>
+    </div>
+  );
+}
