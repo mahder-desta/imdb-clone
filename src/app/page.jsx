937f159026bb75ca218b92ccad25a443f8b29302
Mahder Desta <mahderds2013@gmail.com>
2023-03-25 16:01:13 +0300

    fetch data from tmdb api in homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,24 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+const API_KEY = process.env.API_KEY;
 
-const inter = Inter({ subsets: ["latin"] });
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "trending";
 
-export default function Home() {
-  return <h1 className="text-red-500">Home</h1>;
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre == "topRated" ? "movie/top-rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 1 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to load data");
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
+
+  console.log(results);
+
+  return <div></div>;
 }
